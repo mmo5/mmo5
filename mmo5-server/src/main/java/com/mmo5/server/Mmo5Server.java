a67b45ce77c376eae9b5a8ccf3bc04e368bf2fa7
Yaniv Grama <ygrama@outbrain.com>
2017-04-03 14:31:50 +0300

    increase timeout

--- mmo5-server/src/main/java/com/mmo5/server/Mmo5Server.java
+++ mmo5-server/src/main/java/com/mmo5/server/Mmo5Server.java
@@ -10,6 +10,7 @@ public class Mmo5Server {
   public static void main(String[] args) {
     Spark.port(getHerokuAssignedPort());
     Spark.staticFiles.location("/public"); //index.html is served at localhost:4567 (default port)
+    Spark.webSocketIdleTimeoutMillis(10000000);
     Spark.webSocket("/mmo5", new WebSocketHandler(new GameManager()));
     Spark.get("/", (req, res) -> "MMO5 Server is Up and running!");
     Spark.init();
